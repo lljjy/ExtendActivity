27fb4cd6a70fdfbbd969eea990b985a49bcad509
lljjy <1245265290@qq.com>
2019-05-05 17:24:05 +0800

    first commit

---
+++ app/src/main/java/com/example/extendactivity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.extendactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
